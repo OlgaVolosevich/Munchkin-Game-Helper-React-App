f8c90492621e9b752cd81e655ac04190fe346f0e
Volosevich Olga <o.volosevich96@gmail.com>
2021-10-30 17:53:36 +0300

    feat: add adding of new players

--- src/App.js
+++ src/App.js
@@ -5,14 +5,33 @@ import Form from "./components/Form";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      classOptions: ["Нет класса", "Волшебник", "Вор", "Клирик", "Воин"],
+      raceOptions: ["Нет рассы", "Эльф", "Хафлинг", "Дварф"],
+      players: [],
+    };
   }
-
+  addNewPlayer = (newPlayer) => {
+    const players = [...this.state.players, newPlayer];
+    return this.setState(
+      {
+        players,
+      },
+      () => {
+        console.log(this.state.players, "---players");
+      }
+    );
+  };
   render() {
+    const { classOptions, raceOptions } = this.state;
     return (
       <main>
         <h1>Добавьте игроков</h1>
-        <Form />
+        <Form
+          classOptions={classOptions}
+          raceOptions={raceOptions}
+          addNewPlayer={this.addNewPlayer}
+        />
         <button>Начать игру</button>
       </main>
     );
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,35 +3,98 @@ import { Component } from "react";
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      player: {
+        name: "",
+        gameClass: this.props.classOptions[0],
+        race: this.props.raceOptions[0],
+        gender: null,
+      },
+    };
+  }
+  submitHandler(event) {
+    event.preventDefault();
   }
 
+  changeHandler = (event) => {
+    const player = { ...this.state.player };
+    player[event.target.name] = event.target.value;
+    this.setState({
+      player,
+    });
+  };
+  addPlayerHandler = () => {
+    const player = { ...this.state.player };
+    this.props.addNewPlayer(player);
+  /*  this.resetInputValues()*/
+  };
+ /* resetInputValues = () => {
+    const player = {
+      name: "",
+      gameClass: this.props.classOptions[0],
+      race: this.props.raceOptions[0],
+      gender: null,
+    };
+    this.setState({
+      player,
+    }, () => {
+        console.log(this.state.player)
+    });
+  };*/
   render() {
+    const { classOptions, raceOptions } = this.props;
+    const { name, gameClass, race } = this.state.player;
     return (
-      <form action="/" name="playerInfo">
+      <form action="/" name="playerInfo" onSubmit={this.submitHandler}>
         <label htmlFor="name">Имя</label>
-        <input name="name" id="name" type="text" placeholder="Введите имя" />
+        <input
+          name="name"
+          id="name"
+          type="text"
+          placeholder="Введите имя"
+          value={name}
+          onChange={this.changeHandler}
+        />
         <label htmlFor="race">Расса</label>
-        <select id="race" defaultValue="Нет рассы">
-          <option>Нет рассы</option>
-          <option>Эльф</option>
-          <option>Хафлинг</option>
-          <option>Дварф</option>
+        <select
+          id="race"
+          name="race"
+          defaultValue={race}
+          onChange={this.changeHandler}
+        >
+          {raceOptions.map((option, index) => (
+            <option key={index}>{option}</option>
+          ))}
         </select>
         <label htmlFor="class">Класс</label>
-        <select id="class" defaultValue="Нет класса">
-          <option>Нет класса</option>
-          <option>Волшебник</option>
-          <option>Вор</option>
-          <option>Клирик</option>
-          <option>Воин</option>
+        <select
+          id="gameClass"
+          name="gameClass"
+          defaultValue={gameClass}
+          onChange={this.changeHandler}
+        >
+          {classOptions.map((option, index) => (
+            <option key={index}>{option}</option>
+          ))}
         </select>
         <p>Пол</p>
         <label htmlFor="genderM">Мужской</label>
-        <input type="radio" id="genderM" name="gender" value="male" />
+        <input
+          type="radio"
+          id="genderM"
+          name="gender"
+          value="male"
+          onChange={this.changeHandler}
+        />
         <label htmlFor="genderF">Женский</label>
-        <input type="radio" id="genderF" name="gender" value="female" />
-        <button>Добавить игрока</button>
+        <input
+          type="radio"
+          id="genderF"
+          name="gender"
+          value="female"
+          onChange={this.changeHandler}
+        />
+        <button onClick={this.addPlayerHandler}>Добавить игрока</button>
       </form>
     );
   }
